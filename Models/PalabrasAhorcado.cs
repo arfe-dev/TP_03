2d03a418f677f3b70a71e460b916b48ca26afaec
MATIAS ALBERTO FRAIMAN <49979893@ort>
2026-06-05 08:20:06 -0300

    Model corregido

--- Models/PalabrasAhorcado.cs
+++ Models/PalabrasAhorcado.cs
@@ -1,17 +1,17 @@
 namespace TP_03.Models;
 
 public class PalabrasAhorcado{
-        private List <string> palabras{};
+    private List <string> palabras;
 
     public PalabrasAhorcado(){
-        palabras = new List<string>();
-        palabras.Add("COMPUTADORA", "LABORATORIO", "PROCESADOR", "PROGRAMACION", "GABINETE", "PROFESORES", "ESTUDIANTE", "VISUALSTUDIO");
+        palabras = new List<string>(){
+            "COMPUTADORA", "LABORATORIO", "PROCESADOR", "PROGRAMACION", "GABINETE", "PROFESORES", "ESTUDIANTE", "VISUALSTUDIO"
+        };
     }
 
- public string obtenerPalabra(){
-    Random rnd = new Random();
-    int indice = rnd.Next(palabras.Count);
-    string stringRandom = palabras[indice];
-    return stringRandom;
+    public string obtenerPalabra(){
+        Random rnd = new Random();
+        int indice = rnd.Next(palabras.Count);
+        return palabras[indice];
     }
 }
